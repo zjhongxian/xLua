1a2e9877cf8ef74af6c130696a2915a756ad6b75
zjhongxian <zjhongxian@gmail.com>
2017-06-07 14:55:38 +0800

    add pb.c library and build ios/android xlua lib in mac OS X

--- build/make_android_luajit.sh
+++ build/make_android_luajit.sh
@@ -1,11 +1,11 @@
-export ANDROID_NDK=~/android-ndk-r10e
+export ANDROID_NDK=~/android-ndk-r10d
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SRCDIR=$DIR/luajit-2.1.0b2
-NDK=~/android-ndk-r10e
+NDK=~/android-ndk-r10d
 
 echo "Building armv7 lib"
 NDKVER=$NDK/toolchains/arm-linux-androideabi-4.8  
-NDKP=$NDKVER/prebuilt/linux-x86_64/bin/arm-linux-androideabi-  
+NDKP=$NDKVER/prebuilt/darwin-x86_64/bin/arm-linux-androideabi-  
 NDKARCH="-march=armv7-a -mfloat-abi=softfp -Wl,--fix-cortex-a8"  
 NDKABI=14 
 NDKF="--sysroot $NDK/platforms/android-$NDKABI/arch-arm"
@@ -15,7 +15,7 @@ make HOST_CC="gcc -m32" CROSS=$NDKP TARGET_SYS=Linux TARGET_FLAGS="$NDKF $NDKARC
 
 cd "$DIR"
 mkdir -p build_lj_v7a && cd build_lj_v7a
-cmake -DUSING_LUAJIT=ON -DANDROID_ABI=armeabi-v7a -DCMAKE_TOOLCHAIN_FILE=../cmake/android.toolchain.cmake -DANDROID_TOOLCHAIN_NAME=arm-linux-androideabi-clang3.6 -DANDROID_NATIVE_API_LEVEL=android-9 ../
+cmake -DUSING_LUAJIT=ON -DANDROID_ABI=armeabi-v7a -DCMAKE_TOOLCHAIN_FILE=../cmake/android.toolchain.cmake -DANDROID_TOOLCHAIN_NAME=arm-linux-androideabi-clang3.5 -DANDROID_NATIVE_API_LEVEL=android-9 ../
 cd "$DIR"
 cmake --build build_lj_v7a --config Release
 mkdir -p plugin_luajit/Plugins/Android/libs/armeabi-v7a/
@@ -24,7 +24,7 @@ cp build_lj_v7a/libxlua.so plugin_luajit/Plugins/Android/libs/armeabi-v7a/libxlu
 
 echo "Building x86 lib"
 NDKVER=$NDK/toolchains/x86-4.8  
-NDKP=$NDKVER/prebuilt/linux-x86_64/bin/i686-linux-android-  
+NDKP=$NDKVER/prebuilt/darwin-x86_64/bin/i686-linux-android-  
 NDKABI=14  
 NDKF="--sysroot $NDK/platforms/android-$NDKABI/arch-x86"  
 cd "$SRCDIR"
