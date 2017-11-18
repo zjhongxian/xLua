0e4df09334634367915edc067004b56b8a01fb4c
zjhongxian <zjhongxian@gmail.com>
2017-11-19 01:21:25 +0800

    android/ios/osx 下xlua库编译更新

--- build/make_android_lua53.sh
+++ build/make_android_lua53.sh
@@ -3,7 +3,7 @@ if [ -z "$ANDROID_NDK" ]; then
 fi
 
 mkdir -p build_v7a && cd build_v7a
-cmake -DANDROID_ABI=armeabi-v7a -DCMAKE_TOOLCHAIN_FILE=../cmake/android.toolchain.cmake -DANDROID_TOOLCHAIN_NAME=arm-linux-androideabi-clang3.6 -DANDROID_NATIVE_API_LEVEL=android-9 ../
+cmake -DANDROID_ABI=armeabi-v7a -DCMAKE_TOOLCHAIN_FILE=../cmake/android.toolchain.cmake -DANDROID_TOOLCHAIN_NAME=arm-linux-androideabi-clang3.5 -DANDROID_NATIVE_API_LEVEL=android-9 ../
 cd ..
 cmake --build build_v7a --config Release
 mkdir -p plugin_lua53/Plugins/Android/libs/armeabi-v7a/
